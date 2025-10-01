bc6c03e94ea92466e9bee64171559d469ea21a1a
Clement Pene <cpene@nilgiri>
2025-10-01 10:14:59 +0200

    add config file on /install directory

--- setup.py
+++ setup.py
@@ -11,8 +11,9 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        # install launch files
+        # install launch files and config files
         ('share/' + package_name + '/launch', glob('launch/*.launch.py')),
+        ('share/' + package_name + '/config', glob('config/*.yaml')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
